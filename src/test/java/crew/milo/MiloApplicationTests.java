d2f0b7b244927c14830e0d197257a08c577a1243
ahn-sj <jaesa5221@gmail.com>
2025-03-09 18:22:06 +0900

    init project

---
+++ src/test/java/crew/milo/MiloApplicationTests.java
@@ -0,0 +1,13 @@
+package crew.milo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
